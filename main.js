e6af48edb70b9cda1031586e7782002954bc029f
GdayRui <lirui.jobs@gmail.com>
2019-10-03 21:43:44 +1000

    Organize settings into one centralized object

--- main.js
+++ main.js
@@ -11,11 +11,15 @@ $(document).ready(function(){
 	});
 });
 
-var letterArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-var letterArray_ = '.*';
-
-var colorArray = ['lightpink','lightgreen','lightgray','plum', 'lightsalmon','lightskyblue'];
-var colorArray_ = ['gold','silver'];
+var settings = {
+	letterArray: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+	colorArray: ['lightpink','lightgreen','lightgray','plum', 'lightsalmon','lightskyblue'],
+	interval: 1000,			// after every such interval, a new letter is generated on screen
+	minTravelTime: 1000,
+	travelTimeDiff: 7000,	// the difference between min travel time and max travel time
+	minFontSize: 30,
+	fontSizeDiff: 200		// the difference between min font size and max font size
+}
 
 var isStarted = false;
 var isResized = false;
@@ -30,7 +34,7 @@ function dropLetters(){
 		return;  
 	}
 	
-	var currColor = colorArray[Math.floor(Math.random()*colorArray.length)];
+	var currColor = settings.colorArray[Math.floor(Math.random() * settings.colorArray.length)];
 	
 	if(!isStarted){
 		$('#introduction').hide();
@@ -38,22 +42,18 @@ function dropLetters(){
 		$('#scoreNum').show();
 
 		itv = setInterval(function(){
-			var currLetterIdx = Math.floor(Math.random()*letterArray.length);
-			var currLetter = letterArray[currLetterIdx];
+			var currLetterIdx = Math.floor(Math.random()*settings.letterArray.length);
+			var currLetter = settings.letterArray[currLetterIdx];
 			
-			var currLeft = Math.floor(Math.random()*maxWidth)+30;
+			var currLeft = Math.floor(Math.random() * settings.fontSizeDiff) + settings.minFontSize;
 			var opacity = Math.random();
 			
-			var maxFont = 200;
-			if(window.innerWidth<1000){
-				maxFont = 200;
-			}
-			var fontSize = Math.floor(Math.random()* maxFont)+30;
+			var fontSize = Math.floor(Math.random()* settings.fontSizeDiff) + settings.minFontSize;
 			
 			var elemHtml = '<div id="letter_'+id+'" style="left:'+ currLeft +'px; opacity:'+opacity+'; font-size:'+fontSize+'px;color:'+currColor+'" class="letterDiv">'+currLetter+'</div>'
 			$('.canvas').append(elemHtml);
 			
-			var randSpeed = Math.floor(Math.random()*7000)+1000;
+			var randSpeed = Math.floor(Math.random() * settings.travelTimeDiff) + settings.minTravelTime;
 			
 			var newElem = $('#letter_'+id);
 			
@@ -63,7 +63,7 @@ function dropLetters(){
 			});	
 			
 			id++;
-		}, 1000 );
+		}, settings.interval );
 	}
 	else{
 		clearInterval(itv);
